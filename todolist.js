5ab1d6c7355c17d4fc2f890576374c7f0ae4cc49
SaikalSdd <38737621+SaikalSdd@users.noreply.github.com>
2019-02-27 21:00:17 +0600

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -19,25 +19,5 @@ for (let i = 0; i < all_tasks.length; i++) {
     };
 
 
-/*
-var ul_of_list = document.querySelectorAll("#list");
-var all_tasks = document.querySelectorAll("li");
-
-document.getElementById("add_new").addEventListener('click', () =>{
-    let tasks = document.createElement("LI");
-    tasks.textContent = document.getElementById("task_input").value;
-    document.getElementById('list').appendChild(tasks);
-    document.getElementById("task_input").value = " ";
-})
-
-var index = document.getElementById("list").childNodes
-
-for (let i = 0; i < index.length; i++) {
-    index[i].addEventListener("click", () => {
-        var done = index[i].textContent.strike()
-        index[i].innerHTML = done;
-        });
-    }
- */
 
  
